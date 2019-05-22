cf7229d9a021245b391135387ceeb51df619652f
davpal <paluchdaw@gmail.com>
2019-05-22 16:09:03 +0200

    Make Field Component as function

--- src/Field.js
+++ src/Field.js
@@ -1,15 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Field extends Component {
-    render() {
-        let icon = null;
-        if(this.props.player === 'x')
-            icon = <i className="fas fa-times fa-4x"></i>;
-        else if(this.props.player === 'o')
-            icon = <i className="far fa-circle fa-4x"></i>;
+const Field = (props) => {
+    let icon = null;
+    if(props.player === 'x')
+        icon = <i className="fas fa-times fa-4x"></i>;
+    else if(props.player === 'o')
+        icon = <i className="far fa-circle fa-4x"></i>;
 
-        return <td onClick={this.props.click}>{icon}</td>;
-    }
+    return <td onClick={props.click}>{icon}</td>;
 }
 
 export default Field;
